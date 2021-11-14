eaec49454b326db17727f02373ae3e44c4f8bb7d
vaibhavkumar5 <mrkumarvaibhav2001@gmail.com>
2021-11-14 15:57:27 +0530

    qual sec

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,8 +49,24 @@ skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabsContent = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
 
+        tabsContent.forEach(tabsContent =>{
+            tabsContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
 /*==================== SERVICES MODAL ====================*/
 
 
